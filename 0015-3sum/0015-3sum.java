ddf5932007a1bbf780dc89ca248f63f0178c211e
Akash <codewithakash09@gmail.com>
2026-09-25 15:34:16 +0530

    Time: 37 ms (22.02%), Space: 59.1 MB (74.43%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,56 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first numbers
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+
             int left = i + 1;
             int right = nums.length - 1;
+
             while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
+
                 if (sum == 0) {
-                    ans.add(Arrays.asList(
+
+                    result.add(Arrays.asList(
                         nums[i],
                         nums[left],
                         nums[right]
                     ));
-                    while (left < right &&
-                           nums[left] == nums[left + 1]) {
+
+                    // Skip duplicate second numbers
+                    while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
-                    while (left < right &&
-                           nums[right] == nums[right - 1]) {
+
+                    // Skip duplicate third numbers
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
+
                     left++;
                     right--;
+
                 } else if (sum < 0) {
+
                     left++;
+
                 } else {
+
                     right--;
                 }
             }
         }
-        return ans;
+
+        return result;
     }
 }
